a8f0b9822d83a6ec3bddac4eaf83677435cb72e5
andrewheathcote <andrew.heathcote@newcastle.edu.au>
2025-12-22 21:56:52 +0100

    Add functionality to run_XXX_demo to allow seeting of response, direciton, force_direction, force_response, and rtContaminatnNA Add in some speed results and first set of full R equivilant to C results

--- Censoring Test/likelihood_test_lba cens_trunc_ah.R
+++ Censoring Test/likelihood_test_lba cens_trunc_ah.R
@@ -8,8 +8,6 @@ rm(list=ls())
 library(EMC2)
 source("Censoring Test/test_likelihood_plotfuns_ah.R")
 
-RNGkind("L'Ecuyer-CMRG")
-set.seed(123)
 
 # Whether to run the (slow) MCMC recovery fits
 RUN_FITS <- FALSE
@@ -48,6 +46,10 @@ LBA_cens <- function() {
 run_lba_demo <- function(p_contaminant = 0, cens = TRUE, estimate_contaminant = FALSE,
                          n_trials = 500, UC = NULL, UT=NULL, LC=NULL, LT=NULL,range=1,
                          cores_for_chains = 3,layout=c(2,3),natural=TRUE,n_cores=1,
+                         LCresponse = FALSE, UCresponse = FALSE,
+                         LCdirection = TRUE, UCdirection = TRUE,
+                         force_direction = TRUE, force_response = TRUE,
+                         rtContaminantNA = FALSE,
                          sample_file = "samples_LBA.RData") {
   # if (UC == Inf & p_contaminant>0)
   #   stop("Require a finite UC (deadline) when omissions are present.")
@@ -102,14 +104,13 @@ run_lba_demo <- function(p_contaminant = 0, cens = TRUE, estimate_contaminant = 
     UT = UT,
     LC = LC,
     LT = LT,
-    LCdirection = FALSE, # deadline-only censoring -> rt=Inf (not -Inf)
-    rtContaminantNA = FALSE # contaminant omissions coded same as censored with Inf
+    LCresponse = LCresponse, UCresponse = UCresponse,
+    LCdirection = LCdirection, UCdirection = UCdirection,
+    force_direction = force_direction, force_response = force_response,
+    rtContaminantNA = rtContaminantNA
   )
 
   cat("\n--- LBA demo ---\n")
-  cat("estimate_contaminant =", estimate_contaminant, "\n")
-  cat("p_contaminant (true) =", p_contaminant, "\n")
-  cat("UC =", UC, " n_trials per cell =", n_trials, "\n\n")
 
   cat("Counts by response (NA = omission):\n")
   print(table(dat$R, useNA = "ifany"))
@@ -131,15 +132,15 @@ run_lba_demo <- function(p_contaminant = 0, cens = TRUE, estimate_contaminant = 
   # Profile plots. Use n_cores=1 on Windows.
   library(parallel)
   par(mfrow=layout)
+
   rtr <- system.time({print(profile_plot_test(dat, designLBA, p_vector, n_cores = n_cores, range=range,
     layout = NULL, figure_title = "R Likelihood",natural=natural))})
   cat(paste0("R likelihood: ",round(rtr[3],2),"\n\n"))
 
-
   rtc <- system.time({print(profile_plot_test(dat, designLBA, p_vector, n_cores = n_cores, range=range,
     layout = NULL, use_c = TRUE, figure_title = "C Likelihood",natural=natural))})
   cat(paste0("C likelihood: ",round(rtc[3],2),"\n"))
-  cat(paste0("\nSpeedup: ",round(rtr[3]/rtc[3],1),"\n"))
+  cat(paste0("\nSpeedup: ",round(rtr[3]/rtc[3],2),"\n"))
 
   if (isTRUE(RUN_FITS)) {
     emc <- make_emc(dat, designLBA, type = "single")
@@ -151,15 +152,65 @@ run_lba_demo <- function(p_contaminant = 0, cens = TRUE, estimate_contaminant = 
   invisible(list(data = dat, design = designLBA, true_pars = p_vector))
 }
 
+RNGkind("L'Ecuyer-CMRG")
+set.seed(123)
+
 # Test 1: Equivalence of R and C++ function no censor/trunc/contam ----
+# NB: Here and below cens=FALSE does not call the original Rcpp likelihood
+#     (although it is still in particle_ll.cpp), the cens_trunc version is
+#     ALWAYS called, whereas in R cens does switch between the orignal and
+#     cens_trunc versions.
+
 res_old <- run_lba_demo(
   p_contaminant = 0,
   cens=FALSE,
   estimate_contaminant = FALSE,
   n_trials = 10000,
-  UC = Inf,
+  # UC = Inf,
   sample_file = "samples_lba_old.RData"
 )
+#           true   max   miss
+# B         2.00 1.979  0.021
+# v         1.00 1.015 -0.015
+# v_lMTRUE  1.25 1.269 -0.019
+# A         0.50 0.468  0.032
+# t0        0.20 0.192  0.008
+# sv_lMTRUE 0.80 0.815 -0.015
+# R likelihood: 3.06
+#
+#           true   max   miss
+# B         2.00 1.979  0.021
+# v         1.00 1.015 -0.015
+# v_lMTRUE  1.25 1.269 -0.019
+# A         0.50 0.468  0.032
+# t0        0.20 0.192  0.008
+# sv_lMTRUE 0.80 0.815 -0.015
+# C likelihood: 2.95
+#
+# Speedup: 1.04
+
+# # SPEED UNDER DEV
+#           true   max   miss
+# B         2.00 1.979  0.021
+# v         1.00 1.015 -0.015
+# v_lMTRUE  1.25 1.269 -0.019
+# A         0.50 0.468  0.032
+# t0        0.20 0.192  0.008
+# sv_lMTRUE 0.80 0.815 -0.015
+# R likelihood: 2.92
+#
+#           true   max   miss
+# B         2.00 1.979  0.021
+# v         1.00 1.015 -0.015
+# v_lMTRUE  1.25 1.269 -0.019
+# A         0.50 0.468  0.032
+# t0        0.20 0.192  0.008
+# sv_lMTRUE 0.80 0.815 -0.015
+# C likelihood: 2.4
+#
+# Speedup: 1.22
+
+
 
 # Test 2: Censored RTs ----
 res_cens <- run_lba_demo(
@@ -170,7 +221,6 @@ res_cens <- run_lba_demo(
   sample_file = "samples_lba_cens.RData"
 )
 
-# R version off, but C version great!
 res_cens <- run_lba_demo(
   p_contaminant = 0,
   estimate_contaminant = FALSE,
@@ -179,13 +229,13 @@ res_cens <- run_lba_demo(
   sample_file = "samples_lba_cens.RData"
 )
 
-# But identical here.
+
 res_cens <- run_lba_demo(
   p_contaminant = 0,
   estimate_contaminant = FALSE,
   n_trials = 10000,
   UC = 1.8,
-  LC = .85,
+  LC = .85,n_cores=9,
   sample_file = "samples_lba_cens.RData"
 )
 
@@ -196,6 +246,7 @@ res_contam <- run_lba_demo(
   estimate_contaminant = FALSE,
   n_trials = 10000,
   UT = 1.5,
+  n_cores=9,
   sample_file = "samples_lba_cens_trunc.RData"
 )
 
@@ -203,7 +254,8 @@ res_contam <- run_lba_demo(
   p_contaminant = 0,
   estimate_contaminant = FALSE,
   n_trials = 10000,
-  LT = .9,n_cores=9,
+  LT = .9,
+  n_cores=9,
   sample_file = "samples_lba_cens_trunc.RData"
 )
 
@@ -232,28 +284,52 @@ res_contam <- run_lba_demo(
 )
 
 # Test 5: Contaminant omissions  ------------
+
 res_contam <- run_lba_demo(
-  p_contaminant = 0.15,
+  p_contaminant = 0.05,
   estimate_contaminant = TRUE,
   n_trials = 10000,
   layout=c(2,4),
   sample_file = "samples_lba_contam.RData"
 )
 
-
 res_contam <- run_lba_demo(
   p_contaminant = .05,
   estimate_contaminant = TRUE,
   n_trials = 10000,
   UC = 1.85,
-  LC = .915,
-  UT = 1.95,
   LT = .84,
+  UT = 1.95,
   n_cores=9,
   layout=c(2,4),
   sample_file = "samples_lba_cens_trunc.RData"
 )
 
+res_contam <- run_lba_demo(
+  p_contaminant = .05,
+  estimate_contaminant = TRUE,
+  n_trials = 10000,
+  LC = .915,
+  LT = .84,
+  UT = 1.95,
+  n_cores=9,
+  layout=c(2,4),
+  sample_file = "samples_lba_cens_trunc.RData"
+)
+
+# This case no longer barred by a check, as expected recovery not great but not terrible
+res_contam <- run_lba_demo(
+  p_contaminant = .05,
+  estimate_contaminant = TRUE,
+  n_trials = 10000,
+  LC = .915,
+  UC = 1.85,
+  LT = .84,
+  UT = 1.95,
+  n_cores=9,
+  layout=c(2,4),
+  sample_file = "samples_lba_cens_trunc.RData"
+)
 
 #### RDM ----
 
@@ -280,6 +356,10 @@ RDM_cens <- function() {
 run_RDM_demo <- function(p_contaminant = 0, cens = TRUE, estimate_contaminant = FALSE,
                          n_trials = 500, UC = NULL, UT=NULL, LC=NULL, LT=NULL,range=1,
                          cores_for_chains = 3,layout=c(2,3),natural=TRUE,n_cores=1,
+                         LCresponse = FALSE, UCresponse = FALSE,
+                         LCdirection = TRUE, UCdirection = TRUE,
+                         force_direction = TRUE, force_response = TRUE,
+                         rtContaminantNA = FALSE,
                          sample_file = "samples_RDM.RData") {
   # if (UC == Inf & p_contaminant>0)
   #   stop("Require a finite UC (deadline) when omissions are present.")
@@ -334,14 +414,13 @@ run_RDM_demo <- function(p_contaminant = 0, cens = TRUE, estimate_contaminant = 
     UT = UT,
     LC = LC,
     LT = LT,
-    LCdirection = FALSE, # deadline-only censoring -> rt=Inf (not -Inf)
-    rtContaminantNA = FALSE # contaminant omissions coded same as censored with Inf
+    LCresponse = LCresponse, UCresponse = UCresponse,
+    LCdirection = LCdirection, UCdirection = UCdirection,
+    force_direction = force_direction, force_response = force_response,
+    rtContaminantNA = rtContaminantNA
   )
 
   cat("\n--- RDM demo ---\n")
-  cat("estimate_contaminant =", estimate_contaminant, "\n")
-  cat("p_contaminant (true) =", p_contaminant, "\n")
-  cat("UC =", UC, " n_trials per cell =", n_trials, "\n\n")
 
   cat("Counts by response (NA = omission):\n")
   print(table(dat$R, useNA = "ifany"))
@@ -371,7 +450,7 @@ run_RDM_demo <- function(p_contaminant = 0, cens = TRUE, estimate_contaminant = 
   rtc <- system.time({print(profile_plot_test(dat, designRDM, p_vector, n_cores = n_cores, range=range,
     layout = NULL, use_c = TRUE, figure_title = "C Likelihood",natural=natural))})
   cat(paste0("C likelihood: ",round(rtc[3],2),"\n"))
-  cat(paste0("\nSpeedup: ",round(rtr[3]/rtc[3],1),"\n"))
+  cat(paste0("\nSpeedup: ",round(rtr[3]/rtc[3],2),"\n"))
 
   if (isTRUE(RUN_FITS)) {
     emc <- make_emc(dat, designRDM, type = "single")
@@ -383,7 +462,11 @@ run_RDM_demo <- function(p_contaminant = 0, cens = TRUE, estimate_contaminant = 
   invisible(list(data = dat, design = designRDM, true_pars = p_vector))
 }
 
+RNGkind("L'Ecuyer-CMRG")
+set.seed(123)
+
 # Test 1: Equivalence of R and C++ function no censor/trunc/contam ----
+
 res_old <- run_RDM_demo(
   p_contaminant = 0,
   cens=FALSE,
@@ -392,9 +475,49 @@ res_old <- run_RDM_demo(
   UC = Inf,
   sample_file = "samples_RDM_old.RData"
 )
+#          true   max   miss
+# B         2.0 1.979  0.021
+# v         1.0 1.005 -0.005
+# v_lMTRUE  2.0 2.021 -0.021
+# A         0.5 0.468  0.032
+# t0        0.2 0.192  0.008
+# s_lMTRUE  0.8 0.810 -0.010
+# R likelihood: 3.02
+#
+#          true   max   miss
+# B         2.0 1.979  0.021
+# v         1.0 1.005 -0.005
+# v_lMTRUE  2.0 2.021 -0.021
+# A         0.5 0.468  0.032
+# t0        0.2 0.192  0.008
+# s_lMTRUE  0.8 0.810 -0.010
+# C likelihood: 2.81
+#
+# Speedup: 1.07
+
+# SPEED UNDER DEV
+#          true   max   miss
+# B         2.0 1.979  0.021
+# v         1.0 1.005 -0.005
+# v_lMTRUE  2.0 2.021 -0.021
+# A         0.5 0.468  0.032
+# t0        0.2 0.192  0.008
+# s_lMTRUE  0.8 0.810 -0.010
+# R likelihood: 2.74
+#
+#          true   max   miss
+# B         2.0 1.979  0.021
+# v         1.0 1.005 -0.005
+# v_lMTRUE  2.0 2.021 -0.021
+# A         0.5 0.468  0.032
+# t0        0.2 0.192  0.008
+# s_lMTRUE  0.8 0.810 -0.010
+# C likelihood: 2.39
+#
+# Speedup: 1.15
+
 
 # Test 2: Censored RTs ----
-# R version off, but C version great!
 res_cens <- run_RDM_demo(
   p_contaminant = 0,
   estimate_contaminant = FALSE,
@@ -403,7 +526,6 @@ res_cens <- run_RDM_demo(
   sample_file = "samples_RDM_cens.RData"
 )
 
-# R version off, but C version great!
 res_cens <- run_RDM_demo(
   p_contaminant = 0,
   estimate_contaminant = FALSE,
@@ -412,7 +534,6 @@ res_cens <- run_RDM_demo(
   sample_file = "samples_RDM_cens.RData"
 )
 
-# V worse, t0 and s_lMTrue better
 res_cens <- run_RDM_demo(
   p_contaminant = 0,
   estimate_contaminant = FALSE,
@@ -432,7 +553,6 @@ res_contam <- run_RDM_demo(
   sample_file = "samples_RDM_cens_trunc.RData"
 )
 
-# R worse, C great!
 res_contam <- run_RDM_demo(
   p_contaminant = 0,
   estimate_contaminant = FALSE,
@@ -441,7 +561,6 @@ res_contam <- run_RDM_demo(
   sample_file = "samples_RDM_cens_trunc.RData"
 )
 
-# R much worse, C great!
 res_contam <- run_RDM_demo(
   p_contaminant = 0,
   estimate_contaminant = FALSE,
@@ -454,7 +573,6 @@ res_contam <- run_RDM_demo(
 
 # Test 4: Truncation and censoring ------------
 
-# C way better
 res_contam <- run_RDM_demo(
   p_contaminant = 0,
   estimate_contaminant = FALSE,
@@ -468,6 +586,7 @@ res_contam <- run_RDM_demo(
 )
 
 # Test 5: Contaminant omissions  ------------
+
 res_contam <- run_RDM_demo(
   p_contaminant = 0.15,
   estimate_contaminant = TRUE,
@@ -476,24 +595,55 @@ res_contam <- run_RDM_demo(
   sample_file = "samples_RDM_contam.RData"
 )
 
-
-# C way better
 res_contam <- run_RDM_demo(
   p_contaminant = .05,
   estimate_contaminant = TRUE,
   n_trials = 10000,
-  UC = 1.8,
-  LC = .8,
+  UC = 1.775,
   UT = 2.2,
   LT = .775,
   n_cores=9,
   layout=c(2,4),
-  sample_file = "samples_RDM_cens_trunc.RData"
+  sample_file = "samples_rdm_cens_trunc.RData"
+)
+
+res_contam <- run_RDM_demo(
+  p_contaminant = .05,
+  estimate_contaminant = TRUE,
+  n_trials = 10000,
+  LC = .825,
+  UT = 2.2,
+  LT = .775,
+  n_cores=9,
+  layout=c(2,4),
+  sample_file = "samples_rdm_cens_trunc.RData"
+)
+
+res_contam <- run_RDM_demo(
+  p_contaminant = .05,
+  estimate_contaminant = TRUE,
+  n_trials = 10000,
+  UC = 1.775,
+  LC = .825,
+  UT = 2.2,
+  LT = .775,
+  n_cores=9,
+  layout=c(2,4),
+  sample_file = "samples_rdm_cens_trunc.RData"
 )
 
 
-
 #### LNR ----
+
+# devtools::load_all()
+# debug(run_RDM_demo)
+# undebug(EMC2:::log_likelihood_race_cens_trunc)
+# undebug(profile_plot_test)
+# undebug(lfun)
+# undebug(EMC2:::calc_ll_R)
+# undebug(EMC2:::log_likelihood_race_cens_trunc)
+
+
 
 LNR_no_cens <- function() {
   m <- LNR()
@@ -518,6 +668,10 @@ LNR_cens <- function() {
 run_LNR_demo <- function(p_contaminant = 0, cens = TRUE, estimate_contaminant = FALSE,
                          n_trials = 500, UC = NULL, UT=NULL, LC=NULL, LT=NULL,range=1,
                          cores_for_chains = 3,layout=c(2,3),natural=TRUE,n_cores=1,
+                         LCresponse = FALSE, UCresponse = FALSE,
+                         LCdirection = TRUE, UCdirection = TRUE,
+                         force_direction = TRUE, force_response = TRUE,
+                         rtContaminantNA = FALSE,
                          sample_file = "samples_LNR.RData") {
   # if (UC == Inf & p_contaminant>0)
   #   stop("Require a finite UC (deadline) when omissions are present.")
@@ -569,14 +723,13 @@ run_LNR_demo <- function(p_contaminant = 0, cens = TRUE, estimate_contaminant = 
     UT = UT,
     LC = LC,
     LT = LT,
-    LCdirection = FALSE, # deadline-only censoring -> rt=Inf (not -Inf)
-    rtContaminantNA = FALSE # contaminant omissions coded same as censored with Inf
+    LCresponse = LCresponse, UCresponse = UCresponse,
+    LCdirection = LCdirection, UCdirection = UCdirection,
+    force_direction = force_direction, force_response = force_response,
+    rtContaminantNA = rtContaminantNA
   )
 
   cat("\n--- LNR demo ---\n")
-  cat("estimate_contaminant =", estimate_contaminant, "\n")
-  cat("p_contaminant (true) =", p_contaminant, "\n")
-  cat("UC =", UC, " n_trials per cell =", n_trials, "\n\n")
 
   cat("Counts by response (NA = omission):\n")
   print(table(dat$R, useNA = "ifany"))
@@ -606,7 +759,7 @@ run_LNR_demo <- function(p_contaminant = 0, cens = TRUE, estimate_contaminant = 
   rtc <- system.time({print(profile_plot_test(dat, designLNR, p_vector, n_cores = n_cores, range=range,
     layout = NULL, use_c = TRUE, figure_title = "C Likelihood",natural=natural))})
   cat(paste0("C likelihood: ",round(rtc[3],2),"\n"))
-  cat(paste0("\nSpeedup: ",round(rtr[3]/rtc[3],1),"\n"))
+  cat(paste0("\nSpeedup: ",round(rtr[3]/rtc[3],2),"\n"))
 
   if (isTRUE(RUN_FITS)) {
     emc <- make_emc(dat, designLNR, type = "single")
@@ -618,7 +771,11 @@ run_LNR_demo <- function(p_contaminant = 0, cens = TRUE, estimate_contaminant = 
   invisible(list(data = dat, design = designLNR, true_pars = p_vector))
 }
 
+RNGkind("L'Ecuyer-CMRG")
+set.seed(123)
+
 # Test 1: Equivalence of R and C++ function no censor/trunc/contam ----
+
 res_old <- run_LNR_demo(
   p_contaminant = 0,
   cens=FALSE,
@@ -627,9 +784,44 @@ res_old <- run_LNR_demo(
   UC = Inf,
   sample_file = "samples_LNR_old.RData"
 )
+#            true    max   miss
+# m         0.405  0.391  0.014
+# m_lMTRUE -0.799 -0.811  0.012
+# s         1.000  1.015 -0.015
+# s_lMTRUE  0.800  0.814 -0.014
+# t0        0.200  0.192  0.008
+# R likelihood: 2.09
+#
+#            true    max   miss
+# m         0.405  0.391  0.014
+# m_lMTRUE -0.799 -0.811  0.012
+# s         1.000  1.015 -0.015
+# s_lMTRUE  0.800  0.814 -0.014
+# t0        0.200  0.192  0.008
+# C likelihood: 2.04
+#
+# Speedup: 1.02
+
+# SPEED UNDER DEV
+#            true    max   miss
+# m         0.405  0.391  0.014
+# m_lMTRUE -0.799 -0.811  0.012
+# s         1.000  1.015 -0.015
+# s_lMTRUE  0.800  0.814 -0.014
+# t0        0.200  0.192  0.008
+# R likelihood: 1.7
+#
+#            true    max   miss
+# m         0.405  0.391  0.014
+# m_lMTRUE -0.799 -0.811  0.012
+# s         1.000  1.015 -0.015
+# s_lMTRUE  0.800  0.814 -0.014
+# t0        0.200  0.192  0.008
+# C likelihood: 1.65
+#
+# Speedup: 1.03
 
 # Test 2: Censored RTs ----
-# R version off, but C version great!
 res_cens <- run_LNR_demo(
   p_contaminant = 0,
   estimate_contaminant = FALSE,
@@ -638,12 +830,12 @@ res_cens <- run_LNR_demo(
   sample_file = "samples_LNR_cens.RData"
 )
 
-# R version off, but C version great!
 res_cens <- run_LNR_demo(
   p_contaminant = 0,
   estimate_contaminant = FALSE,
   n_trials = 10000,
   LC = .45,
+  n_cores=9,
   sample_file = "samples_LNR_cens.RData"
 )
 
@@ -652,6 +844,7 @@ res_cens <- run_LNR_demo(
   p_contaminant = 0,
   estimate_contaminant = FALSE,
   n_trials = 10000,
+  n_cores=9,
   UC = 1.6,
   LC = .4,
   sample_file = "samples_LNR_cens.RData"
@@ -663,7 +856,8 @@ res_contam <- run_LNR_demo(
   p_contaminant = 0,
   estimate_contaminant = FALSE,
   n_trials = 10000,
-  UT = 1.4,n_cores=9,
+  UT = 1.4,
+  n_cores=9,
   sample_file = "samples_LNR_cens_trunc.RData"
 )
 
@@ -672,7 +866,8 @@ res_contam <- run_LNR_demo(
   p_contaminant = 0,
   estimate_contaminant = FALSE,
   n_trials = 10000,
-  LT = .45,n_cores=9,
+  LT = .45,
+  n_cores=9,
   sample_file = "samples_LNR_cens_trunc.RData"
 )
 
@@ -689,7 +884,6 @@ res_contam <- run_LNR_demo(
 
 # Test 4: Truncation and censoring ------------
 
-# C way better
 res_contam <- run_LNR_demo(
   p_contaminant = 0,
   estimate_contaminant = FALSE,
@@ -703,16 +897,36 @@ res_contam <- run_LNR_demo(
 )
 
 # Test 5: Contaminant omissions  ------------
+
 res_contam <- run_LNR_demo(
   p_contaminant = 0.15,
   estimate_contaminant = TRUE,
   n_trials = 10000,
-  layout=c(2,4),
   sample_file = "samples_LNR_contam.RData"
 )
 
+res_contam <- run_LNR_demo(
+  p_contaminant = .05,
+  estimate_contaminant = TRUE,
+  n_trials = 10000,
+  UC = 1.5,
+  UT = 1.7,
+  LT = .375,
+  n_cores=9,
+  sample_file = "samples_rdm_cens_trunc.RData"
+)
 
-# C way better
+res_contam <- run_LNR_demo(
+  p_contaminant = .05,
+  estimate_contaminant = TRUE,
+  n_trials = 10000,
+  LC = .425,
+  UT = 1.7,
+  LT = .375,
+  n_cores=9,
+  sample_file = "samples_rdm_cens_trunc.RData"
+)
+
 res_contam <- run_LNR_demo(
   p_contaminant = .05,
   estimate_contaminant = TRUE,
@@ -722,6 +936,5 @@ res_contam <- run_LNR_demo(
   UT = 1.7,
   LT = .375,
   n_cores=9,
-  layout=c(2,4),
-  sample_file = "samples_LNR_cens_trunc.RData"
+  sample_file = "samples_rdm_cens_trunc.RData"
 )
