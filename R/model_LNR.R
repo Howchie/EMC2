6f8b7bb09884a100116adea7dbdb975b5fbb9acc
andrewheathcote <andrew.heathcote@newcastle.edu.au>
2025-12-22 21:50:32 +0100

    Change LNR and RDM models to _cens_trunc likelihoods

--- R/model_LNR.R
+++ R/model_LNR.R
@@ -101,8 +101,9 @@ LNR <- function() {
     # Probability function (CDF) for single accumulator
     pfun=function(rt,pars) pLNR(rt,pars),
     # Race likelihood combining pfun and dfun
-    log_likelihood=function(pars,dadm, model, min_ll=log(1e-10))
-      log_likelihood_race(pars=pars, dadm = dadm, model = model, min_ll = min_ll)
+    log_likelihood=function(pars,dadm,model,min_ll=log(1e-10)){
+      log_likelihood_race_cens_trunc(pars=pars, dadm = dadm, model = model, min_ll = min_ll)
+    }
   )
 }
 
--- R/model_RDM.R
+++ R/model_RDM.R
@@ -391,7 +391,8 @@ RDM <- function(){
     # Probability function (CDF) for single accumulator
     pfun=function(rt,pars) pRDM(rt,pars),
     # Race likelihood combining pfun and dfun
-    log_likelihood=function(pars,dadm,model,min_ll=log(1e-10))
-      log_likelihood_race(pars=pars, dadm = dadm, model = model, min_ll = min_ll) # left normal for testing censoring logic
+    log_likelihood=function(pars,dadm,model,min_ll=log(1e-10)){
+      log_likelihood_race_cens_trunc(pars=pars, dadm = dadm, model = model, min_ll = min_ll)
+    }
   )
 }
