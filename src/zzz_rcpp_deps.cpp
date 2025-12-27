544341804477ad975b1a5c6195f8a830d2ed9684
andrewheathcote <andrew.heathcote@newcastle.edu.au>
2025-12-27 18:29:36 +0100

    New src file to make sure RcppArmadillo.h compiled before RcppEigen.h and RcppNumerical.h

---
+++ src/zzz_rcpp_deps.cpp
@@ -0,0 +1,6 @@
+// [[Rcpp::depends(RcppArmadillo, RcppEigen)]]
+// (order matters: Armadillo first)
+// [[Rcpp::plugins(cpp17)]]
+#include <RcppArmadillo.h>
+#include <RcppEigen.h>
+#include <RcppNumerical.h>
