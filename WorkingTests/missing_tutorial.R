239043edd1d3459f4b8c281da5dee6d3472d4917
andrewheathcote <andrew.heathcote@newcastle.edu.au>
2026-05-09 17:59:35 +0200

    Extra examples

--- WorkingTests/missing_tutorial.R
+++ WorkingTests/missing_tutorial.R
@@ -122,6 +122,48 @@ make_missing(dat,LT=.69,LC=.76,UC=1.59,UT=1.71)
 make_data(p_vector, designRDM,n_trials = 2,
           TC=list(LT=.69,LC=.76,UC=1.59,UT=1.71,rt_resolution=1/60))
 
+# Note that we can also indicate truncation and censoring in design
+
+designRDM <- design(model = RDM,UC=1.5,
+  factors = list(subjects = 1:2, S = c("left", "right")),Rlevels = c("left", "right"),
+  matchfun = function(d) as.numeric(d$S) == as.numeric(d$lR),
+  formula = list(B ~ 1, v ~ lM, A ~ 1, t0 ~ 1, s ~ lM, pContaminant~1),
+  constants = c(s = log(1)))
+
+# and that will apply to both make data
+dat <- make_data(p_vector, designRDM,n_trials=1e4)
+
+# and to make_emc
+dat <- dat[,1:5]  # Remove UC column
+dUC <- make_emc(dat,designRDM,type="single")
+head(dUC[[1]]$data[[1]]$UC)
+
+# If the data already has censoring etc. indicated the design value will not be
+# be used
+dat$UC <- 2
+dUC <- make_emc(dat,designRDM,type="single")
+head(dUC[[1]]$data[[1]]$UC)
+
+# Note that the data re checked to make sure the values make sense
+dat$UC <- 1
+dUC <- make_emc(dat,designRDM,type="single")
+
+# Note also that if the data has censoring or truncation columns they will not
+# be picked up by design (as those columns would be specific to a particular
+# number of trials), instead a value must be passed explicitly.
+designRDM <- design(model = RDM,UC=1.5,data=dat,
+  matchfun = function(d) as.numeric(d$S) == as.numeric(d$lR),
+  formula = list(B ~ 1, v ~ lM, A ~ 1, t0 ~ 1, s ~ lM, pContaminant~1),
+  constants = c(s = log(1)))
+
+designRDM$UC
+
+# Now when we give that design to make_data it adds in the UC value from the
+# design.
+dat$UC <- NULL
+dUC <- make_emc(dat,designRDM,type="single")
+head(dUC[[1]]$data[[1]]$UC)
+
 
 #### Check profiles ----
 
